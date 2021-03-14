df167c6e4c681a23c156bcf509dd6a81cef1d0c7
Will Roberts <willroberts3@protonmail.com>
2021-03-14 13:50:44 -0400

    Adds graceful shutdown, helper functions

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,10 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net::{Shutdown, TcpStream};
 use std::sync::atomic::{AtomicI32, Ordering};
 
 use crate::message;
+
+const MAX_MESSAGE_SIZE: usize = 4110; // https://wiki.vg/Rcon#Fragmentation
 
 pub struct Client {
 	conn: TcpStream,
@@ -15,6 +17,18 @@ impl Client {
 			conn: TcpStream::connect(hostport).unwrap(),
 			last_id: AtomicI32::new(0),
 		}
+	}
+
+	pub fn close(&mut self) {
+		self.conn.shutdown(Shutdown::Both).unwrap();
+	}
+
+	pub fn authenticate(&mut self, password: String) -> message::Message {
+		self.send_message(message::MessageType::Authenticate as i32, password)
+	}
+
+	pub fn send_command(&mut self, command: String) -> message::Message {
+		self.send_message(message::MessageType::Command as i32, command)
 	}
 
 	fn next_id(&self) -> i32 {
@@ -34,7 +48,7 @@ impl Client {
 		};
 
 		self.conn.write_all(&message::encode_message(req)[..]).unwrap();
-		let mut resp_bytes = [0u8; 4096];
+		let mut resp_bytes = [0u8; MAX_MESSAGE_SIZE];
 		self.conn.read(&mut resp_bytes).unwrap();
 		let resp = message::decode_message(resp_bytes.to_vec());
 
@@ -48,17 +62,22 @@ impl Client {
 mod tests {
 	use super::*;
 
+	const DEFAULT_HOSTPORT: &str = "127.0.0.1:25575";
+	const DEFAULT_PASSWORD: &str = "minecraft";
+
 	#[test]
 	fn test_id_generation() {
-		let client = Client::new("127.0.0.1:25575".to_string());
+		let mut client = Client::new(DEFAULT_HOSTPORT.to_string());
 		assert_eq!(client.next_id(), 1);
 		assert_eq!(client.next_id(), 2);
 		assert_eq!(client.next_id(), 3);
+		client.close();
 	}
 	
 	#[test]
 	fn test_send_message() {
-		let mut client = Client::new("127.0.0.1:25575".to_string());
-		client.send_message(message::MessageType::Authenticate as i32, "minecraft".to_string());
+		let mut client = Client::new(DEFAULT_HOSTPORT.to_string());
+		client.send_message(message::MessageType::Authenticate as i32, DEFAULT_PASSWORD.to_string());
+		client.close();
 	}
 }
