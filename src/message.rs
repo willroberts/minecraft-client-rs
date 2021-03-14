0a7371a052ad7f3690eb7e513e35389a60b20312
Will Roberts <willroberts3@protonmail.com>
2021-03-13 22:18:16 -0500

    Cleanup

--- src/message.rs
+++ src/message.rs
@@ -20,11 +20,12 @@ pub struct Message {
 
 pub fn encode_message(msg: Message) -> Vec<u8> {
 	let mut bytes: Vec<u8> = vec!();
-	bytes.extend_from_slice(&msg.size.to_le_bytes()); // Message size.
-	bytes.extend_from_slice(&msg.id.to_le_bytes()); // Message ID.
-	bytes.extend_from_slice(&msg.msg_type.to_le_bytes()); // Message type.
-	bytes.extend_from_slice(msg.body.as_bytes()); // Message body.
-	bytes.extend_from_slice(&[0, 0]); // Terminator.
+
+	bytes.extend_from_slice(&msg.size.to_le_bytes());
+	bytes.extend_from_slice(&msg.id.to_le_bytes());
+	bytes.extend_from_slice(&msg.msg_type.to_le_bytes());
+	bytes.extend_from_slice(msg.body.as_bytes());
+	bytes.extend_from_slice(&[0, 0]);
 
 	bytes
 }
@@ -33,8 +34,8 @@ pub fn decode_message(bytes: Vec<u8>) -> Message {
 	let size = i32::from_le_bytes(bytes[0..4].try_into().expect("invalid message size"));
 	let id = i32::from_le_bytes(bytes[4..8].try_into().expect("invalid message id"));
 	let msg_type = i32::from_le_bytes(bytes[8..12].try_into().expect("invalid message type"));
-	let mut body = "".to_string();
 
+	let mut body = "".to_string();
 	let body_len: usize = (size - HEADER_SIZE).try_into().expect("invalid message body length");
 	if body_len > 0 {
 		body = from_utf8(&bytes[12..12+body_len]).unwrap().to_string();
